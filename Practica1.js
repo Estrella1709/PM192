cf9ae5ff4d377531dd6507427d8f846d1ebefac5
Estrella1709 <estrellatorrijos17@gmail.com>
2025-05-19 17:19:50 -0600

    Ejercicio B - Fuction a Arrow

--- Practica1.js
+++ Practica1.js
@@ -6,3 +6,9 @@ nombre = "Ana María";
 const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 console.log(saludo);
 
+const cuadrado = numero => numero*numero;
+console.log(cuadrado(2));
+console.log(cuadrado(3));
+console.log(cuadrado(5));
+
+
