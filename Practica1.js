efad9024cb244a641da17a7c4b3b03076a1de7e3
Estrella1709 <estrellatorrijos17@gmail.com>
2025-05-19 17:44:35 -0600

    Ejercicio C - Arrow function

--- Practica1.js
+++ Practica1.js
@@ -1,10 +1,10 @@
-let nombre = "Armando";
+ let nombre = "Armando";
 const edad = 25;
 
 nombre = "Ana María";
 
 const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
-console.log(saludo);
+console.log(saludo); 
 
 const cuadrado = numero => numero*numero;
 console.log(cuadrado(2));
@@ -12,3 +12,8 @@ console.log(cuadrado(3));
 console.log(cuadrado(5));
 
 
+let nombre1 = "Isay";
+const edad1 = 37;
+
+const saludo1 =(nombre1, edad1)=>("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
+console.log(saludo1(nombre1, edad1));
